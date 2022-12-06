61c8c819d5da4542e16ef8a9e6e5be916c16fa6d
alimbekov17 <orozaliorozali@gmail.com>
2022-12-06 13:37:08 +0100

    commit

--- src/task23/Main.java
+++ src/task23/Main.java
@@ -1,9 +1,6 @@
 package task23;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,64 +17,31 @@ public class Main {
         }
 
 
-        int[] sortedArray = new int[array.length];
-        sort(array, sortedArray);
         System.out.println("Array       : " + Arrays.toString(array));
-        System.out.println("Sorted Array: " + Arrays.toString(sortedArray));
+        sort(array);
+        System.out.println("Sorted Array: " + Arrays.toString(array));
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-        ArrayList<Integer> sortedArrayList = new ArrayList<>();
-        sort(arrayList, sortedArrayList);
         System.out.println("ArrayList       : " + arrayList);
-        System.out.println("Sorted ArrayList: " + sortedArrayList);
+        sort(arrayList);
+        System.out.println("Sorted ArrayList: " + arrayList);
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-        LinkedList<Integer> sortedLinkedList = new LinkedList<>();
-        sort(linkedList, sortedLinkedList);
         System.out.println("LinkedList       : " + linkedList);
-        System.out.println("Sorted LinkedList: " + sortedLinkedList);
+        sort(linkedList);
+        System.out.println("Sorted LinkedList: " + linkedList);
 
     }
 
-    public static void sort(LinkedList<Integer> linkedList, LinkedList<Integer> sortedLinkedList) {
-        for (Integer integer : linkedList) {
-            if (integer == 0){
-                sortedLinkedList.add(integer);
-            }
-        }
-        for (Integer integer : linkedList) {
-            if (integer == 1){
-                sortedLinkedList.add(integer);
-            }
-        }
+    public static void sort(LinkedList<Integer> linkedList) {
+        Collections.sort(linkedList);
     }
 
-    public static void sort(ArrayList<Integer> arrayList, ArrayList<Integer> sortedArrayList) {
-        for (Integer integer : arrayList) {
-            if (integer == 0){
-                sortedArrayList.add(integer);
-            }
-        }
-        for (Integer integer : arrayList) {
-            if (integer == 1){
-                sortedArrayList.add(integer);
-            }
-        }
+    public static void sort(ArrayList<Integer> arrayList) {
+        Collections.sort(arrayList);
     }
 
-
-    public static void sort(int[] array, int[] sortedArray) {
-        int a = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 0) {
-                sortedArray[i] = array[i];
-                a++;
-            }
-        }
-        for (int j : array) {
-            if (j == 1) {
-                sortedArray[a++] = j;
-            }
-        }
+    public static void sort(int[] array) {
+        Arrays.sort(array);
     }
+
 }
